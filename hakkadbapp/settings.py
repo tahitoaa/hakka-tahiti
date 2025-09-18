b62c119bf08b1a069e15cdb0e64e40dbeb5210d8
tahitoaa <tahitoa.arbelot@gmail.com>
2025-09-17 21:30:10 -1000

    try fix static

--- hakkadbapp/settings.py
+++ hakkadbapp/settings.py
@@ -17,7 +17,6 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
